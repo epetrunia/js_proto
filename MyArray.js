dee3feb6630e9f3660d6bed616caf781cbc57d7b
Eugenia Petrunia <avepetrunia@gmail.com>
2020-07-12 12:59:49 +0300

    Create function isMyArray()

--- MyArray.js
+++ MyArray.js
@@ -6,4 +6,11 @@ function MyArray() {
     }
 
     this.length = 0;
+
+    this.isMyArray = function isMyArray() {
+        return this instanceof MyArray;
+    }
 }
+
+const myArrProto = new MyArray();
+MyArray.prototype = myArrProto;
---
+++ index.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const arr1 = new MyArray();
+console.log(`arr1.isMyArray -> ${arr1.isMyArray()}`);
